97e2f7f7656ca075f7f0cda90eab2239a181d220
marinel <marinel.bosna00@e-uvt.ro>
2022-01-12 15:55:11 +0200

    Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student';
+import { StudentService } from './student.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'studentManagementSystem';
+export class AppComponent implements OnInit {
+  public students: Student[];
+  public editStudent: Student | undefined;
+  public deleteStudent: Student | undefined;
+
+
+  constructor(private studentService: StudentService) {
+    this.students = [];
+
+  }
+
+  ngOnInit() {
+    this.getStudents();
+  }
+
+  public getStudents(): void {
+    this.studentService.getStudents().subscribe(
+      (response: Student[]) => {
+        this.students = response;
+        console.log(this.students);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddEmloyee(addForm: NgForm): void {
+    // @ts-ignore
+    document.getElementById('add-student-form').click();
+    this.studentService.addStudent(addForm.value).subscribe(
+      (response: Student) => {
+        console.log(response);
+        this.getStudents();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateEmloyee(student: Student): void {
+    this.studentService.updateStudent(student).subscribe(
+      (response: Student) => {
+        console.log(response);
+        this.getStudents();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmloyee(studentId: number): void {
+    this.studentService.deleteStudent(studentId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getStudents();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchStudents(key: string): void {
+    console.log(key);
+    const results: Student[] = [];
+    for (const student of this.students) {
+      if (student.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || student.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || student.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || student.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(student);
+      }
+    }
+    this.students = results;
+    if (results.length === 0 || !key) {
+      this.getStudents();
+    }
+  }
+
+  public onOpenModal( mode: string, student?: Student): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addStudentModal');
+    }
+    if (mode === 'edit') {
+      this.editStudent = student;
+      button.setAttribute('data-target', '#updateStudentModal');
+    }
+    if (mode === 'delete') {
+      this.deleteStudent = student;
+      button.setAttribute('data-target', '#deleteStudentModal');
+    }
+    // @ts-ignore
+    container.appendChild(button);
+    button.click();
+  }
+
+
+
 }
---
+++ src/app/student.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from './student';
+import { environment } from 'src/environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class StudentService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(`${this.apiServerUrl}/students/all`);
+  }
+
+  public addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(`${this.apiServerUrl}/students/add`, student);
+  }
+
+  public updateStudent(student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.apiServerUrl}/students/update`, student);
+  }
+
+  public deleteStudent(studentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/students/delete/${studentId}`);
+  }
+}
